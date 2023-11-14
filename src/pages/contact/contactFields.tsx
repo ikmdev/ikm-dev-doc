1560824bf2804ebddd66bc69cb3eedcc48b233de
Sebastian Oliver <seoliver@deloitte.com>
2023-11-14 14:15:23 -0500

    set state contact

--- src/pages/contact/contactFields.tsx
+++ src/pages/contact/contactFields.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import clsx from 'clsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
@@ -8,53 +7,66 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import useBaseUrl, {useBaseUrlUtils} from '@docusaurus/useBaseUrl';
 import emailjs from 'emailjs-com';
+import React, { useState } from 'react';
 
 export function ContactUs() {
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-    
-      const serviceID = 'service_35ed30c';  // Replace with your Service ID from EmailJS
-      const templateID = 'template_jkoy4bi';  // Replace with your Template ID from EmailJS
-      const userID = 'h1V68vf3GYZLE32EE';  // Replace with your User ID from EmailJS
-    
-      const templateParams = {
-        from_name: event.target.name.value,
-        reply_to: event.target._replyto.value,
-        message: event.target.message.value,
-        to_email: 'seb.burner.mail@gmail.com'
-      };
-    
-      emailjs.send(serviceID, templateID, templateParams, userID)
-        .then((response) => {
-           console.log('Email sent successfully', response.status, response.text);
-        }, (error) => {
-           console.log('Email sending failed', error);
-        });
-    };
-    
-  
-    return (
-      <div className={styles.contactContainer}>
-        <h1>Get in Touch</h1>
-  
-        <form onSubmit={handleSubmit}>
-            <div className={styles.inputGroup}>
-                <input type="text" id="name" name="name" required placeholder="First Name" />
-                <input type="text" id="name" name="name" required placeholder="Last Name" />
-                
-            </div>
-            <div className={styles.inputGroup}>
-            <input type="email" id="_replyto" name="_replyto" required placeholder="Email" />
-            
-            </div>
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submitStatus, setSubmitStatus] = useState('');
 
-            
-  
-            <textarea id="message" name="message" rows={5} required placeholder="What's on your mind..."></textarea>
-            <br />
-            <input type="submit" value="Send" />
-        </form>
-      </div>
-    );
-  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsSubmitting(true);
+    setSubmitStatus('Sending...');
+
+    const serviceID = 'service_35ed30c';  // Replace with your Service ID from EmailJS
+    const templateID = 'template_jkoy4bi';  // Replace with your Template ID from EmailJS
+    const userID = 'h1V68vf3GYZLE32EE';  // Replace with your User ID from EmailJS
+
+    var fullName = event.target.firstName.value + " " + event.target.lastName.value;
+    const templateParams = {
+      from_name: fullName,
+      reply_to: event.target._replyto.value,
+      message: event.target.message.value,
+      to_email: 'seb.burner.mail@gmail.com'
+    };
+
+    emailjs.send(serviceID, templateID, templateParams, userID)
+      .then((response) => {
+        setIsSubmitting(false);
+        setSubmitStatus('Sent!');
+      }, (error) => {
+        setIsSubmitting(false);
+        setSubmitStatus('Failed to send');
+      });
+  };
+
+  const renderButtonContent = () => {
+    if (isSubmitting) {
+      return 'Sending...';
+    } else if (submitStatus) {
+      return submitStatus;
+    } else {
+      return 'Send';
+    }
+  };
+
+  return (
+    <div className={styles.contactContainer}>
+      <h1>Get in Touch</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div className={styles.inputGroup}>
+          <input type="text" id="firstName" name="firstName" required placeholder="First Name" />
+          <input type="text" id="lastName" name="lastName" required placeholder="Last Name" />
+        </div>
+        <div className={styles.inputGroup}>
+          <input type="email" id="_replyto" name="_replyto" required placeholder="Email" />
+        </div>
+
+        <textarea id="message" name="message" rows={5} required placeholder="What's on your mind..."></textarea>
+        <br />
+        <input type="submit" value={renderButtonContent()} disabled={isSubmitting} />
+      </form>
+    </div>
+  );
+}
